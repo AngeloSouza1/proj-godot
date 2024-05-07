7aa70207b3417552a45cd59bfe082e94b672c68f
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-07 10:30:00 -0300

    feat: Criação do projeto Hello World

---
+++ Hello World/player.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+@export var speed = 600.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello World from Player!")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	position +=  input * speed * delta
+	pass
