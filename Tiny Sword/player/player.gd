22ba46b9f970139a77a0422c23ca98e4afeab38c
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-13 16:10:19 -0300

    chore: ajuste da estrutura do codigo

--- Tiny Sword/player/player.gd
+++ Tiny Sword/player/player.gd
@@ -4,11 +4,29 @@ extends CharacterBody2D
 @export var speed: float = 3
 @onready var sprite: Sprite2D = $Sprite2D
 
+var input_vector: Vector2 =  Vector2(0, 0)
 var is_running: bool = false
+var was_running: bool = false
 var is_attacking: bool = false
 var attack_cooldown: float= 0.0
 
 func _process(delta: float) -> void:
+	# Ler input
+	read_input()
+
+	# Processar ataque
+	update_attack_cooldown(delta)
+
+	# Ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+	# Processar animação e rotação de sprite
+	play_run_idle_animation()
+	rotate_sprite()
+
+
+func update_attack_cooldown(delta: float) -> void:
 	# Atualizar temporizador do ataque
 	if is_attacking:
 		attack_cooldown -= delta  # 0.06 -  0.016 = 0.0584 exemplo
@@ -18,54 +36,30 @@ func _process(delta: float) -> void:
 			animation_player.play("idle")
 
 func _physics_process(delta: float) -> void:
-	# Obter o input_Vector 
-	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
-	
-	# Apagar deadzone do input vector
-	var deadzone = 0.15
-	if abs(input_vector.x) < 0.15:
-		input_vector.x = 0.0
-	if abs(input_vector.y) < 0.15:
-		input_vector.y = 0.0
-	
-	
-
 	# Modificar a velocidade
 	var	target_velocity = input_vector * speed * 100.0
 	if is_attacking:
 		target_velocity *= 0.25
 	velocity = lerp(velocity, target_velocity, 0.05)
 	move_and_slide()	
-		
-	# Atualizar a is_runnig
-	var was_runnig = is_running
-	is_running = not input_vector.is_zero_approx()	
-		
-	# Tocar animação
-	if not is_attacking:
-		if was_runnig != is_running:
-			if is_running:
-				animation_player.play("run")
-			else:
-				animation_player.play("idle")
-	
-	# Girar sprite
-	if input_vector.x > 0:
-		sprite.flip_h = false
-		# Desmarcar flip_h do Sprite2D
-	elif input_vector.x < 0:  # Certifique-se de usar 'x' minúsculo aqui
-		# Marcar flip_h do Sprite2D
-		sprite.flip_h = true
 
-# Ataque
-	if Input.is_action_just_pressed("attack"):
-		attack()
-		
-		
+func read_input() -> void:
+		# Obter o input_Vector 
+	input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	# Apagar deadzone do input vector
+	var deadzone = 0.15
+	if abs(input_vector.x) < 0.15:
+		input_vector.x = 0.0
+	if abs(input_vector.y) < 0.15:
+		input_vector.y = 0.0		
+  # Atualizar a is_runnig
+	was_running = is_running
+	is_running = not input_vector.is_zero_approx()	
+
 func attack() -> void:
 	if is_attacking:
 		return
-
 	# Tocar animação
 	animation_player.play("attack_side_1")
 	
@@ -74,3 +68,22 @@ func attack() -> void:
 	
 	# Marcar ataque
 	is_attacking = true
+
+func play_run_idle_animation() -> void:
+	#Tocar animação
+	if not is_attacking:
+		if was_running != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
+
+func rotate_sprite() -> void:
+		# Girar sprite
+	if input_vector.x > 0:
+		sprite.flip_h = false
+		# Desmarcar flip_h do Sprite2D
+	elif input_vector.x < 0:  # Certifique-se de usar 'x' minúsculo aqui
+		# Marcar flip_h do Sprite2D
+		sprite.flip_h = true			
+
