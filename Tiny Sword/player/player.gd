8dda13a232a29ed423b2b5f471f4703291e6d9f8
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-13 15:31:47 -0300

    feat: funçao ataque player

--- Tiny Sword/player/player.gd
+++ Tiny Sword/player/player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
 @export var speed: float = 3
-
 @onready var sprite: Sprite2D = $Sprite2D
 
 var is_running: bool = false
+var is_attacking: bool = false
+var attack_cooldown: float= 0.0
 
+func _process(delta: float) -> void:
+	# Atualizar temporizador do ataque
+	if is_attacking:
+		attack_cooldown -= delta  # 0.06 -  0.016 = 0.0584 exemplo
+		if attack_cooldown <= 0.0: 
+			is_attacking = false
+			is_running = false
+			animation_player.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Obter o input_Vector 
@@ -21,9 +29,11 @@ func _physics_process(delta: float) -> void:
 		input_vector.y = 0.0
 	
 	
-	print(input_vector)
+
 	# Modificar a velocidade
 	var	target_velocity = input_vector * speed * 100.0
+	if is_attacking:
+		target_velocity *= 0.25
 	velocity = lerp(velocity, target_velocity, 0.05)
 	move_and_slide()	
 		
@@ -32,11 +42,12 @@ func _physics_process(delta: float) -> void:
 	is_running = not input_vector.is_zero_approx()	
 		
 	# Tocar animação
-	if was_runnig != is_running:
-		if is_running:
-			animation_player.play("run")
-		else:
-			animation_player.play("idle")
+	if not is_attacking:
+		if was_runnig != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
 	
 	# Girar sprite
 	if input_vector.x > 0:
@@ -46,6 +57,20 @@ func _physics_process(delta: float) -> void:
 		# Marcar flip_h do Sprite2D
 		sprite.flip_h = true
 
+# Ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
 		
 		
+func attack() -> void:
+	if is_attacking:
+		return
 
+	# Tocar animação
+	animation_player.play("attack_side_1")
+	
+	# Configurar temporizador
+	attack_cooldown = 0.6
+	
+	# Marcar ataque
+	is_attacking = true
