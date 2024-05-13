9e88f6ea0d17ca6e0901fc43114b2df032a33e98
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-13 13:52:10 -0300

    chore: codigo de movimentação player

--- Tiny Sword/player/player.gd
+++ Tiny Sword/player/player.gd
@@ -2,16 +2,50 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var speed: float = 3
+
+@onready var sprite: Sprite2D = $Sprite2D
+
 var is_running: bool = false
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+func _physics_process(delta: float) -> void:
+	# Obter o input_Vector 
+	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	# Apagar deadzone do input vector
+	var deadzone = 0.15
+	if abs(input_vector.x) < 0.15:
+		input_vector.x = 0.0
+	if abs(input_vector.y) < 0.15:
+		input_vector.y = 0.0
+	
+	
+	print(input_vector)
+	# Modificar a velocidade
+	var	target_velocity = input_vector * speed * 100.0
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()	
+		
+	# Atualizar a is_runnig
+	var was_runnig = is_running
+	is_running = not input_vector.is_zero_approx()	
+		
+	# Tocar animação
+	if was_runnig != is_running:
 		if is_running:
-			animation_player.play("idle")
-			is_running = false
-		else:
 			animation_player.play("run")
-			is_running = true
+		else:
+			animation_player.play("idle")
 	
-	
+	# Girar sprite
+	if input_vector.x > 0:
+		sprite.flip_h = false
+		# Desmarcar flip_h do Sprite2D
+	elif input_vector.x < 0:  # Certifique-se de usar 'x' minúsculo aqui
+		# Marcar flip_h do Sprite2D
+		sprite.flip_h = true
+
+		
+		
+
