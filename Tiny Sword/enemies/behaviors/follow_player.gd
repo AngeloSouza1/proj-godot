5e9937e181685e4c453a0eaba8535b644baee611
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-15 13:00:05 -0300

    feat: funcao do dano ao player

---
+++ Tiny Sword/enemies/behaviors/follow_player.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@export var speed: float = 1
+
+var enemy: Enemy
+var sprite: AnimatedSprite2D
+
+
+func _ready():
+	enemy = get_parent()
+	sprite = enemy.get_node("AnimatedSprite2D")
+
+func _physics_process(delta: float) -> void:
+	# Calcular direção
+	var player_position =GameManager.player_position
+	var difference = player_position - enemy.position
+	var input_vector = difference.normalized()
+	 
+	# Movimento
+	enemy.velocity = input_vector * speed * 100.0
+	enemy.move_and_slide()
+
+		# Girar sprite
+	if input_vector.x > 0:
+		sprite.flip_h = false
+		# Desmarcar flip_h do Sprite2D
+	elif input_vector.x < 0:  # Certifique-se de usar 'x' minúsculo aqui
+		# Marcar flip_h do Sprite2D
+		sprite.flip_h = true			
+
+
---
+++ Tiny Sword/player/player.gd
@@ -0,0 +1,155 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var speed: float = 3
+@export var sword_damage: float = 2
+@export var health: int = 100
+@export var death_prefab: PackedScene
+
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var sword_area: Area2D = $SwordArea
+@onready var hitbox_area: Area2D = $HitboxArea
+
+var input_vector: Vector2 =  Vector2(0, 0)
+var is_running: bool = false
+var was_running: bool = false
+var is_attacking: bool = false
+var attack_cooldown: float= 0.0
+var hitbox_cooldown: float= 0.0
+
+func _process(delta: float) -> void:
+	GameManager.player_position = position
+	# Ler input
+	read_input()
+
+	# Processar ataque
+	update_attack_cooldown(delta)
+
+	# Ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+	# Processar animação e rotação de sprite
+	play_run_idle_animation()
+	if not is_attacking:
+		rotate_sprite()
+	# Processar dano
+	update_hitbox_detection(delta)
+	
+
+func update_attack_cooldown(delta: float) -> void:
+	# Atualizar temporizador do ataque
+	if is_attacking:
+		attack_cooldown -= delta  # 0.06 -  0.016 = 0.0584 exemplo
+		if attack_cooldown <= 0.0: 
+			is_attacking = false
+			is_running = false
+			animation_player.play("idle")
+
+func _physics_process(delta: float) -> void:
+	# Modificar a velocidade
+	var	target_velocity = input_vector * speed * 100.0
+	if is_attacking:
+		target_velocity *= 0.25
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()	
+
+func read_input() -> void:
+		# Obter o input_Vector 
+	input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	# Apagar deadzone do input vector
+	var deadzone = 0.15
+	if abs(input_vector.x) < 0.15:
+		input_vector.x = 0.0
+	if abs(input_vector.y) < 0.15:
+		input_vector.y = 0.0		
+  # Atualizar a is_runnig
+	was_running = is_running
+	is_running = not input_vector.is_zero_approx()	
+
+func attack() -> void:
+	if is_attacking:
+		return
+	# Tocar animação
+	animation_player.play("attack_side_1")
+	
+	# Configurar temporizador
+	attack_cooldown = 0.6
+	
+	# Marcar ataque
+	is_attacking = true
+	
+func deal_damage_to_enemies() -> void:
+	var bodies = sword_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("enemies"):
+			var enemy: Enemy = body
+			# Espaço do vetor para calcular os ataques
+			var direction_to_enemy = (enemy.position - position).normalized()
+			var attack_direction: Vector2
+			if sprite.flip_h:
+				attack_direction = Vector2.LEFT
+			else:
+				attack_direction = Vector2.RIGHT
+			var dot_product = direction_to_enemy.dot(attack_direction)			
+			if dot_product >= 0.3:
+				enemy.damage(sword_damage)
+	
+func play_run_idle_animation() -> void:
+	#Tocar animação
+	if not is_attacking:
+		if was_running != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
+
+func rotate_sprite() -> void:
+		# Girar sprite
+	if input_vector.x > 0:
+		sprite.flip_h = false
+		# Desmarcar flip_h do Sprite2D
+	elif input_vector.x < 0:  # Certifique-se de usar 'x' minúsculo aqui
+		# Marcar flip_h do Sprite2D
+		sprite.flip_h = true			
+
+func update_hitbox_detection(delta: float) -> void:
+	# Temporizador
+	hitbox_cooldown -= delta
+	if hitbox_cooldown > 0: return
+	
+	# Frequencia 
+	hitbox_cooldown = 0.5
+	
+	# Detectar inimigos
+	var bodies =  sword_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("enemies"):
+			var enemy: Enemy = body
+			var damage_amount = 1
+			damage(damage_amount)
+
+func damage(amount: int) -> void:
+	if health <= 0: return
+	health -= amount
+	print("Player recebeu dano de ", amount, ". A vida total é de ", health)
+	#Piscar node 
+	modulate = Color.RED
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.3)
+	
+	#Processar morte
+	if health <= 0: 
+		die()
+	
+func die() -> void:
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+	print("Player morreu!")	
+	queue_free()	
