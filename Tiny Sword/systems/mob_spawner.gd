2336ffc3521e070d81c514ec352f5aca251a8d5a
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-23 11:35:01 -0300

    feat: definindo o limite do cenário

--- Tiny Sword/systems/mob_spawner.gd
+++ Tiny Sword/systems/mob_spawner.gd
@@ -16,11 +16,22 @@ func _process(delta: float):
 	# Frequência
 	var interval = 60.0 / mobs_per_minute
 	cooldown = interval
-	# Instanciar uma criatura aletaroria
+	
+	# Checar se o ponto é válido
+	var point = get_point()
+	var world_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = point
+	parameters.collision_mask = 0b1001
+	var result: Array = world_state.intersect_point(parameters, 1)
+	if not result.is_empty(): return
+
+
+	# Instanciar uma criatura aleatoria
 	var index = randi_range(0, creatures.size()-1)
 	var creature_scene = creatures[index]
 	var creature = creature_scene.instantiate()
-	creature.global_position = get_point()
+	creature.global_position = point
 	get_parent().add_child(creature)
 	
 
